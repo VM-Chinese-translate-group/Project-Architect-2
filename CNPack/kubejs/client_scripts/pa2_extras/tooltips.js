a4d70e717dd562ca1806194603febb81dd08aba5
nageih <zhwlang2008@outlook.com>
2025-02-01 21:39:00 +0800

    update

--- CNPack/kubejs/client_scripts/pa2_extras/tooltips.js
+++ CNPack/kubejs/client_scripts/pa2_extras/tooltips.js
@@ -18,127 +18,127 @@ const tooltipToItem = [
 	{
 		// Dyeable fire information
 		items: "minecraft:flint_and_steel",
-		tips: ["§eYou can make colored fire by right-clicking the fire with a dye", "§8§oColored fire does not make sound, cause damage, or emit particles"]
+		tips: ["tooltip.minecraft.flint_and_steel.1", "tooltip.minecraft.flint_and_steel.2"]
 	},
 	{
 		// Tips for dimensions
 		items: "rftoolsdim:realized_dimension_tab",
-		tips: "§eYou can delete a dimension in the Dimension Editor, using a TNT"
+		tips: ["tooltip.rftoolsdim.realized_dimension_tab.1"]
 	},
 	{
 		// Advanced Wireless Transmitter - Incompatibilities
 		items: "rebornstorage:advanced_wireless_transmitter",
-		tips: "§cDoes not work with Infinity Range Booster, and it doesn't seem to work with Dimension Card either"
+		tips: ["tooltip.rebornstorage.advanced_wireless_transmitter.1"]
 	},
 	{
 		// RS Controller tips
 		items: ["refinedstorage:controller", /refinedstorage:.+_controller/],
-		tips: "§eIf you have issues with accessing RS from Wireless sources or others, try changing the controller to another chunk"
+		tips: ["tooltip.refinedstorage.controller.1"]
 	},
 	{
 		// Extra Info for Rainbow furnaces
 		items: "ironfurnaces:million_furnace",
-		tips: "§eWill smelt an entire stack in one go!"
+		tips: ["tooltip.ironfurnaces.million_furnace.1"]
 	},
 	{
 		// Sneak + Break info
 		items: ["projecte:dm_pedestal", /projecte:.+_matter_block/, /projectexpansion:.+_matter_block/],
-		tips: "§eYou can break the block while sneaking to break it without specific tools"
+		tips: ["tooltip.projecte.dm_pedestal.1"]
 	},
 	{
 		// EMC Links + Interface
 		items: [/projectexpansion:.+_emc_link/, "projectexpansion:transmutation_interface"],
-		tips: ["", "§6Turns off when the player is offline. This is intended by the mod, use a chest or similar as a buffer!"]
+		tips: ["tooltip.projectexpansion.emc_link.1", "tooltip.projectexpansion.emc_link.2"]
 	},
 	{
 		// EMC Links
 		items: /projectexpansion:.+_emc_link/,
-		tips: "§eSneak+Right-click with a different EMC link to replace it (the filter is preserved)"
+		tips: ["tooltip.projectexpansion.emc_link.3"]
 	},
 	{
 		// EMC Links
 		items: "projectexpansion:transmutation_interface",
-		tips: "§eSneak+Right-click with an item will learn it, but won't consume it (does not work with Tome of Knowledge)"
+		tips: ["tooltip.projectexpansion.transmutation_interface.1"]
 	},
 	{
 		// Awakened Draconium joke to make people aware of the Dust's EMC
 		items: "draconicevolution:awakened_draconium_block",
-		tips: "§eDid you know that the ingots from this block are highly grindeable?"
+		tips: ["tooltip.draconicevolution.awakened_draconium_block.1"]
 	},
 	{
 		// Functional Storage upgrades can be cycled with right click now
 		items:  ["functionalstorage:puller_upgrade", "functionalstorage:pusher_upgrade", "functionalstorage:collector_upgrade"],
-		tips: "§7Right clicking the item in hand, without a target, will cycle the entire stack"
+		tips: ["tooltip.functionalstorage.puller_upgrade.1"]
 	},
 	{
 		// Bee Queen Egg
 		items: ["the_bumblezone:bee_queen_spawn_egg"],
-		tips: ["", "§bQueen bees love §4§lArchimeat§b!"]
+		tips: ["tooltip.the_bumblezone.bee_queen_spawn_egg.1", "tooltip.the_bumblezone.bee_queen_spawn_egg.2"]
 	},
 	{
 		// EMC Stars
 		items: [/projecte:klein_star_.+/, /projectexpansion:(magnum|colossal|gargantuan)_star_.+/],
-		tips: "§eYou can charge your star in the left side of your transmutation table"
+		tips: ["tooltip.projecte.klein_star.1"]
 	},
 	{
 		// Items that require EMC Stars
 		items: ["projecte:swiftwolf_rending_gale", "projecte:ignition_ring", "projecte:body_stone", "projecte:soul_stone", "projecte:life_stone", "projecte:volcanite_amulet"],
-		tips: ["", "§eRequires a charged EMC star in your inventory (or curios) to power this item"]
+		tips: ["tooltip.projecte.swiftwolf_rending_gale.1", "tooltip.projecte.swiftwolf_rending_gale.2"]
 	},
 	{
 		// Archium to the Queen
 		items: "kubejs:archium",
-		tips: "§3Something worth of royalty!"
+		tips: ["tooltip.kubejs.archium.1"]
 	},
 	{
 		// Building Gadgets bug warning
 		items: ["buildinggadgets2:gadget_copy_paste", "buildinggadgets2:gadget_cut_paste"],
-		tips: "§cUsing this with tile entities can cause crashes! (Ender Storage blocks, some Create things, etc.)"
+		tips: ["tooltip.buildinggadgets2.gadget_copy_paste.1"]
 	},
 	{
 		// Absorbers
 		items: /rftoolsdim:.+_absorber/,
-		tips: "§eIf the absorber doesn't work the target might be disabled, check the dimlet on the workbench"
+		tips: ["tooltip.rftoolsdim.absorber.1"]
 	},
 	{
 		// Fireworks
 		items: ["minecraft:firework_rocket", "minecraft:firework_star"],
-		tips: "§eRecipe might not appear in JEI, look it up in MC's Wiki, the vanilla one works"
+		tips: ["tooltip.minecraft.firework_rocket.1"]
 	},
 	{
 		// Fireworks
 		items: "minecraft:firework_rocket",
-		tips: "§eThere is a custom recipe added to facilitate autocrafting with different systems"
+		tips: ["tooltip.minecraft.firework_rocket.1"]
 	},
 	{
 		// Sophisticated Upgrades
 		items: ["sophisticatedstorage:crafting_upgrade", "sophisticatedbackpacks:crafting_upgrade"],
-		tips: "§cSome crafts might cause you to crash when using the upgrade, use with caution"
+		tips: ["tooltip.sophisticatedstorage.crafting_upgrade.1"]
 	},
 	{
 		// AE2 Cables
 		items: /ae2:.+_cable/,
-		tips: "§eCan §bsneak + right-click§e on another cable to replace them. Dense only works with other dense."
+		tips: ["tooltip.ae2.cable.1"]
 	},
 	{
 		// Mek Cables
 		items: /mekanism:.+_(pipe|cable|tube|transporter|conductor)/,
-		tips: "§eCan §bsneak + right-click§e on another cable of the same type, to replace it."
+		tips: "tooltip.mekanism.pipe.1"
 	},
 	{
 		// Trader
 		items: ["easy_villagers:trader", "easy_villagers:auto_trader"],
-		tips: "§eRight click with the workstation block to add it to the block!"
+		tips: ["tooltip.easy_villagers.trader.1"]
 	},
 	{
 		// Camo Upgrade - MR
 		items: ["modularrouters:camouflage_upgrade"],
-		tips: "§cUsing this with any cable related thing from AE2 will make the router invisible and inaccessible"
+		tips: ["tooltip.modularrouters.camouflage_upgrade.1"]
 	},
 	{
 		// Void Motor
 		items: "createutilities:void_motor",
-		tips: "§cThis can sometimes cause TPS and/or Lag issues, use carefully"
+		tips: ["tooltip.createutilities.void_motor.1"]
 	}
 ];
 
@@ -154,22 +154,21 @@ const nameFixer = {
 // The tooltip event
 ItemEvents.tooltip(event => {
 	// General tooltips
-	tooltipToItem.forEach(entry => {
-		event.add(entry.items, entry.tips);
-	});
+    tooltipToItem.forEach(entry => {
+    	event.add(entry.items, entry.tips.map(tip => Text.translate(tip)));
+    });
 
 	// Fix PExp tooltips
-	if (fixPExpTooltips)
-	{
-		event.addAdvanced(/projectexpansion:.+/, (item, adv, text) => {
-			let index = text.indexOf(text.find(test => test.getString().startsWith("See the wiki")));
-			if (index === -1)
-				return;
+    if (fixPExpTooltips) {
+        event.addAdvanced(/projectexpansion:.+/, (item, adv, text) => {
+            let index = text.indexOf(text.find(test => test.getString().startsWith("See the wiki")));
+            if (index === -1)
+                return;
 
-			text.set(index, Text.of("More info at §b§nhttps://github.com/DonovanDMC/ProjectExpansion/wiki").yellow());
-		});
-	};
-	
+            text.set(index, Text.of("More info at §b§nhttps://github.com/DonovanDMC/ProjectExpansion/wiki").yellow());
+        });
+    };
+
 	// Fixes incorrect names
 	Object.keys(nameFixer).forEach(item => {
 		event.addAdvanced(item, (stack, adv, text) => text.set(0, nameFixer[item].replace("%name", text.get(0).getString())));
